017308d6cb03c3f55b1030caabccc84d88ff3c65
mateuszskibickiplt <mateusz.skibicki@prettylittlething.com>
2019-12-23 10:19:31 +0000

    styling, scss, buttons, homepage header

--- src/components/header-image/HeaderImage.tsx
+++ src/components/header-image/HeaderImage.tsx
@@ -3,12 +3,16 @@ import React from "react";
 interface IProps {
   img: string;
   title: string;
+  description?: string;
+  children?: any;
   size: "xsmall" | "small" | "medium" | "big";
 }
 
 export const HeaderImage: React.FC<IProps> = ({
   img,
   title,
+  description,
+  children,
   size
 }: IProps): JSX.Element => {
   let classes = "";
@@ -17,9 +21,19 @@ export const HeaderImage: React.FC<IProps> = ({
   size === "medium" && (classes = "header-img header-img__medium");
   size === "big" && (classes = "header-img header-img__big");
 
+  if (children)
+    return (
+      <div className={classes} style={{ backgroundImage: `url(${img})` }}>
+        {children}
+      </div>
+    );
+
   return (
     <div className={classes} style={{ backgroundImage: `url(${img})` }}>
       <h1 className="header-img__title">{title}</h1>
+      {description && (
+        <h2 className="header-img__description">{description}</h2>
+      )}
     </div>
   );
 };
