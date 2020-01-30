5bfd7358de504d60564b48dfe0f0ebea6d6db57e
mateuszskibickiplt <mateusz.skibicki@prettylittlething.com>
2020-01-30 20:07:47 +0000

    seo head changes

--- src/components/article-single/ArticleSingleLayout.tsx
+++ src/components/article-single/ArticleSingleLayout.tsx
@@ -6,7 +6,9 @@ import { HeaderLogo } from "../header-image/HeaderLogo";
 import { Spinner } from "../common/spinner/Spinner";
 import { RichTextComponent } from "../rich-text/RichText";
 import bgImg from "../../assets/imgs/clientele.jpg";
+import profileImg from "../../assets/imgs/profile.jpg";
 import { CommonPage } from "../../helpers/common-page/commonPageHelpers";
+import { Link } from "react-router-dom";
 
 // uid: string | null;
 // id: string | null;
@@ -39,10 +41,12 @@ export const ArticleSingleLayout: React.FC<IProps> = ({
       {/* SEO */}
       <h1 className="d-none">{title}</h1>
       <h2 className="d-none">{title}</h2>
-      {/* SEO */}
       <Helmet>
         <meta property="og:type" content="website" />
+        {date && <meta property="article:published_time" content={date} />}
+        {date && <meta property="article:modified_time" content={date} />}
       </Helmet>
+      {/* SEO end */}
       <HeaderImage img={bgImg} size="medium" position="left">
         <HeaderLogo />
         <div className="container">
@@ -61,14 +65,44 @@ export const ArticleSingleLayout: React.FC<IProps> = ({
         <div className="container px-3 py-5">
           <div className="row justify-content-center">
             <div className="col-12 col-md-10 col-lg-9 px-3">
-              <h3 className="mb-4 text-white letter-spacing-1">
+              <h3 className="mb-2 text-white letter-spacing-1">
                 {description}
               </h3>
-              <h4 className="font-weight-600 text-white">{date}</h4>
+              {date && (
+                <h5 className="font-weight-600 text-white letter-spacing-2 mb-5">
+                  <i>{date}</i>
+                </h5>
+              )}
               <RichTextComponent
                 content={content_text}
                 color="white"
               ></RichTextComponent>
+            </div>
+            <div className="col-12">
+              <div className="row justify-content-center align-items-center py-4">
+                <div className="col-8 col-sm-6 col-md-3 col-lg-2 mb-3">
+                  <img
+                    src={profileImg}
+                    alt="Jacqueline Burns - Gay Matchmaker - The Echelon Scene"
+                    className="shadow rounded-circle img-fluid"
+                  />
+                </div>
+                <div className="col-12 col-sm-8 col-md-7 col-lg-6">
+                  <p className="text-white letter-spacing-1 mb-2">
+                    By Jacqueline Burns
+                  </p>
+                  <p className="text-white letter-spacing-1 mb-2">
+                    Founder of{" "}
+                    <Link to="/" className="text-red">
+                      The Echelon Scene
+                    </Link>
+                  </p>
+                  <p className="text-white letter-spacing-1 mb-2">
+                    The Echelon Scene is an offline gay matchmaking agency for
+                    eligible gay men seeking a long term partner
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/components/layout/SEOhead.tsx
+++ src/components/layout/SEOhead.tsx
@@ -14,75 +14,43 @@ const HeadSEO: React.FC<any> = (): JSX.Element | null => {
     title = null,
     description = null,
     keywords = null,
-    // geo_region = null,
-    // robots = null,
-    item_prop_name = null,
-    item_prop_description = null,
-    twitter_site = null,
-    twitter_title = null,
-    twitter_description = null,
-    twitter_creator = null,
-    og_title = null,
-    // og_type = null,
-    og_url = null,
-    og_description = null,
-    article_section = null,
-    article_tag = null,
     fb_admins = null,
     item_prop_image = null,
     twitter_image = null,
-    og_image = null,
-    article_published_time = null,
-    article_modified_time = null
+    og_image = null
   } = SEO.current;
 
   return (
     <Helmet>
-      {/* random */}
-      {/* <meta name="geo.region" content="GB" /> */}
+      {/* default */}
       <meta name="twitter:card" content="summary" />
-      <meta property="og:site_name" content="The Echelon Scene" />
+      <meta name="twitter:site" content="@TheEchelonScene" />
+      <meta name="twitter:creator" content="@TheEchelonScene" />
+      <meta
+        property="og:site_name"
+        content="The Echelon Scene - Gay Matchmaking"
+      />
       <meta property="og:locale" content="en_GB" />
-      {/* {robots && <meta name="robots" content={robots} />} */}
-      {/* optional */}
+      <meta property="og:url" content={"https://theechelonscene.com"} />
+
+      {/* titles */}
       {title && <title>{title}</title>}
       {title && <meta name="title" content={title} />}
+      {title && <meta itemProp="name" content={title} />}
+      {title && <meta name="twitter:title" content={title} />}
+      {title && <meta property="og:title" content={title} />}
+
+      {/* descriptions */}
       {description && <meta name="description" content={description} />}
+      {description && <meta itemProp="description" content={description} />}
+      {description && <meta name="twitter:description" content={description} />}
+      {description && <meta property="og:description" content={description} />}
+
+      {/* keywords */}
       {keywords && <meta name="keywords" content={keywords} />}
-      {item_prop_name && <meta itemProp="name" content={item_prop_name} />}
-      {item_prop_description && (
-        <meta itemProp="description" content={item_prop_description} />
-      )}
-      {twitter_site && <meta name="twitter:site" content={twitter_site} />}
-      {twitter_title && <meta name="twitter:title" content={twitter_title} />}
-      {twitter_description && (
-        <meta name="twitter:description" content={twitter_description} />
-      )}
-      {twitter_creator && (
-        <meta name="twitter:creator" content={twitter_creator} />
-      )}
-      {/* {og_type && <meta property="og:type" content={og_type} />} */}
-      {og_title && <meta property="og:title" content={og_title} />}
-      {og_url && <meta property="og:url" content={og_url} />}
-      {og_description && (
-        <meta property="og:description" content={og_description} />
-      )}
-      {article_published_time && (
-        <meta
-          property="article:published_time"
-          content={article_published_time}
-        />
-      )}
-      {article_modified_time && (
-        <meta
-          property="article:modified_time"
-          content={article_modified_time}
-        />
-      )}
-      {article_section && (
-        <meta property="article:section" content={article_section} />
-      )}
-      {article_tag && <meta property="article:tag" content={article_tag} />}
+      {keywords && <meta property="article:tag" content={keywords} />}
+
+      {/* the rest */}
       {fb_admins && <meta property="fb:admins" content={fb_admins} />}
       {item_prop_image && <meta itemProp="image" content={item_prop_image} />}
       {twitter_image && <meta name="twitter:image" content={twitter_image} />}
