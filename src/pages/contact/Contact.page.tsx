8b7e4b4e3b700c7915c691e3ddeca677d6b76044
mateuszskibicki <skibickiengland@gmail.com>
2021-02-22 09:16:03 +0000

    captcha

--- src/pages/contact/Contact.page.tsx
+++ src/pages/contact/Contact.page.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { HeaderImage } from "../../components/header-image/HeaderImage";
 import { HeaderLogo } from "../../components/header-image/HeaderLogo";
 import { SEOImgHeaders } from "../../components/common/SEO/SEOImgHeaders";
@@ -11,6 +12,10 @@ import axios from "axios";
 
 const Contact: React.FC<any> = (): JSX.Element | null => {
   const api_token = "pjY7DkRfUrGzbYAXGpmt";
+
+  // Captcha
+
+  const [key, setKey] = useState<null | string>(null)
 
   // Form data
   const [formData, setFormData] = useState({
@@ -83,7 +88,6 @@ const Contact: React.FC<any> = (): JSX.Element | null => {
         }
       );
       if (res && (res.status === 200 || res.status === 201)) {
-        console.log(res);
         setPending(false);
         setSuccess(true);
       }
@@ -106,102 +110,111 @@ const Contact: React.FC<any> = (): JSX.Element | null => {
         {success ? (
           <SuccessResponseBlock />
         ) : (
-          <div className="row justify-content-center my-5">
-            <div className="col-12 col-md-10 col-lg-8">
-              <h4 className="letter-spacing-2 text-white mb-2">
-                Gain a better understanding of our service
+            <div className="row justify-content-center my-5">
+              <div className="col-12 col-md-10 col-lg-8">
+                <h4 className="letter-spacing-2 text-white mb-2">
+                  Gain a better understanding of our service
               </h4>
-              <p className="letter-spacing-1 text-white mb-4">
-                Please fill in the information below
-              </p>
-              <div className="contact-form">
-                {/* Inpu radio */}
-                <div className="form-group mb-4">
-                  <OptionsRadioButton
-                    onClick={onOptionButtonChange}
-                    active={formData.custom_field_197950 === "call"}
-                    name="Call"
-                    value="call"
-                  />
-                  <OptionsRadioButton
-                    onClick={onOptionButtonChange}
-                    active={formData.custom_field_197950 === "email"}
-                    name="Email"
-                    value="email"
-                  />
-                  <OptionsRadioButton
-                    onClick={onOptionButtonChange}
-                    active={formData.custom_field_197950 === "facetoface"}
-                    name="Meet with Jacqueline"
-                    value="facetoface"
-                  />
-                </div>
-                {/* Form inputs */}
-                <div className="form-group mb-4">
-                  <input
-                    type="text"
-                    className="form-control letter-spacing-2"
-                    name="first_name"
-                    placeholder="First Name:"
-                    onChange={onTextInputChange}
-                  />
-                  <input
-                    type="text"
-                    className="form-control letter-spacing-2"
-                    name="last_name"
-                    placeholder="Last Name:"
-                    onChange={onTextInputChange}
-                  />
-                  <input
-                    type="text"
-                    className="form-control letter-spacing-2"
-                    name="phone_numbers"
-                    placeholder="Phone Number:"
-                    onChange={onTextInputChange}
-                  />
-                  <input
-                    type="email"
-                    className="form-control letter-spacing-2"
-                    name="secondary_emails"
-                    placeholder="Email:"
-                    onChange={onTextInputChange}
-                  />
-                  <input
-                    type="text"
-                    className="form-control letter-spacing-2"
-                    name="country"
-                    placeholder="Country:"
-                    onChange={onTextInputChange}
-                  />
-                </div>
-
                 <p className="letter-spacing-1 text-white mb-4">
-                  The details are kept strictly confidential.
+                  Please fill in the information below
+              </p>
+                <div className="contact-form">
+                  {/* Inpu radio */}
+                  <div className="form-group mb-4">
+                    <OptionsRadioButton
+                      onClick={onOptionButtonChange}
+                      active={formData.custom_field_197950 === "call"}
+                      name="Call"
+                      value="call"
+                    />
+                    <OptionsRadioButton
+                      onClick={onOptionButtonChange}
+                      active={formData.custom_field_197950 === "email"}
+                      name="Email"
+                      value="email"
+                    />
+                    <OptionsRadioButton
+                      onClick={onOptionButtonChange}
+                      active={formData.custom_field_197950 === "facetoface"}
+                      name="Meet with Jacqueline"
+                      value="facetoface"
+                    />
+                  </div>
+                  {/* Form inputs */}
+                  <div className="form-group mb-4">
+                    <input
+                      type="text"
+                      className="form-control letter-spacing-2"
+                      name="first_name"
+                      placeholder="First Name:"
+                      onChange={onTextInputChange}
+                    />
+                    <input
+                      type="text"
+                      className="form-control letter-spacing-2"
+                      name="last_name"
+                      placeholder="Last Name:"
+                      onChange={onTextInputChange}
+                    />
+                    <input
+                      type="text"
+                      className="form-control letter-spacing-2"
+                      name="phone_numbers"
+                      placeholder="Phone Number:"
+                      onChange={onTextInputChange}
+                    />
+                    <input
+                      type="email"
+                      className="form-control letter-spacing-2"
+                      name="secondary_emails"
+                      placeholder="Email:"
+                      onChange={onTextInputChange}
+                    />
+                    <input
+                      type="text"
+                      className="form-control letter-spacing-2"
+                      name="country"
+                      placeholder="Country:"
+                      onChange={onTextInputChange}
+                    />
+                  </div>
+
+                  <p className="letter-spacing-1 text-white mb-4">
+                    The details are kept strictly confidential.
                 </p>
 
-                {formErrors && (
-                  <p className="letter-spacing-1 text-red mb-4">{formErrors}</p>
-                )}
+                  {formErrors && (
+                    <p className="letter-spacing-1 text-red mb-4">{formErrors}</p>
+                  )}
 
-                {pending ? (
-                  <button
-                    className="btn btn-white-outline shadow rounded"
-                    onClick={() => console.log("Pending...")}
-                  >
-                    Please wait...
-                  </button>
-                ) : (
-                  <button
-                    className="btn btn-white-outline shadow rounded"
-                    onClick={onEnquireClick}
-                  >
-                    Enquire
-                  </button>
-                )}
+                  <div className="mb-1">
+                    <ReCAPTCHA
+                      sitekey="6LfYwWIaAAAAAOErZXPr9k4WDtYsrlwleuw2xEA9"
+                      onChange={(captcha) => setKey(captcha)}
+                    />
+                  </div>
+
+                  {pending && (
+                    <button
+                      className="btn btn-white-outline shadow rounded"
+                      onClick={() => console.log("Pending...")}
+                    >
+                      Please wait...
+                    </button>
+                  )}
+
+                  {!pending && key && (
+                    <button
+                      className="btn btn-white-outline shadow rounded"
+                      onClick={onEnquireClick}
+                    >
+                      Enquire
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
       </div>
     </div>
   );
