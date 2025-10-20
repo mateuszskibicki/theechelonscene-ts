f3ccba01cfad0e69e38b5c86868eb3ef2fdd28e7
Mateusz Skibicki-Moirai <skibickiengland@gmail.com>
2025-10-20 11:01:06 +0100

    Testing

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -52,222 +52,223 @@ export const Footer: React.FC = (): JSX.Element => {
         <div className="container">
           {/* Mobile only */}
           <div className="row d-md-none">
-          <div className="col-12">
-            <div className="row d-flex flex-row justify-content-center">
-              <FooterSocialIcons />
-            </div>
-          </div>
-
-          <div className="col-12 text-center my-4">
-            <Link to="/contact" className="btn btn--small btn-white-outline">
-              ENQUIRE
-            </Link>
-          </div>
-
-          <div className="col-12">
-            <div className="row flex-row justify-content-center px-3">
-              <div className="col-4 px-1 text-center">
-                <Link to="/" className="text-white text-xs footer__link ">
-                  Home
-                </Link>
-              </div>
-              <div className="col-4 px-1 text-center mb-4">
-                <Link
-                  to="/clients"
-                  className="text-white text-xs footer__link "
-                >
-                  Clientele
-                </Link>
-              </div>
-              <div className="col-4 px-1 text-center mb-4">
-                <Link
-                  to="/founder"
-                  className="text-white text-xs footer__link "
-                >
-                  Founder
-                </Link>
-              </div>
-              <div className="col-4 px-1 text-center mb-4">
-                <Link
-                  to="/how-we-work"
-                  className="text-white text-xs footer__link "
-                >
-                  How we work
-                </Link>
-              </div>
-              <div className="col-4 px-1 text-center mb-4">
-                <Link to="/levels" className="text-white text-xs footer__link ">
-                  Levels
-                </Link>
-              </div>
-              <div className="col-4 px-1 text-center mb-4">
-                <Link to="/blog" className="text-white text-xs footer__link ">
-                  Blog
-                </Link>
-              </div>
-              <div className="col-4 px-1 text-center mb-4">
-                <Link
-                  to="/reviews"
-                  className="text-white text-xs footer__link "
-                >
-                  Reviews
-                </Link>
-              </div>
-              <div className="col-4 px-1 text-center mb-4">
-                <Link to="/press" className="text-white text-xs footer__link ">
-                  Press
-                </Link>
-              </div>
-              <div className="col-4 px-1 text-center mb-4">
-                <Link
-                  to="/locations"
-                  className="text-white text-xs footer__link "
-                >
-                  Locations
-                </Link>
+            <div className="col-12">
+              <div className="row d-flex flex-row justify-content-center">
+                <FooterSocialIcons />
               </div>
             </div>
-          </div>
 
-          <div className="col-12 text-center mb-3">
-                     <button
-              onClick={openModal}
-              className="btn btn--xsmall btn-white-outline newsletter-trigger"
-            >
-              JOIN THE NEWSLETTER
-            </button>
-          </div>
+            <div className="col-12 text-center my-4">
+              <Link to="/contact" className="btn btn--small btn-white-outline">
+                ENQUIRE
+              </Link>
+            </div>
 
-          <div className="col-12 text-center mb-3">
-            <Link to="/tc" className="text-white text-xs footer__link">
-              Terms and conditions
-            </Link>
-          </div>
-
-          <div className="col-12">
-            <p className="text-xs text-white mb-0 text-center">
-              Copyright © {new Date().getFullYear()} The Echelon Scene
-            </p>
-          </div>
-        </div>
-
-        {/* desktop */}
-        <div className="row d-none d-md-flex">
-          {location.pathname !== "/" && (
-            <div className="col-12 mb-3">
-              <div className="row justify-content-center">
-                <div className="col-2 col-xl-1 text-center">
-                  <Link to="/" className="text-white text-xs footer__link">
+            <div className="col-12">
+              <div className="row flex-row justify-content-center px-3">
+                <div className="col-4 px-1 text-center">
+                  <Link to="/" className="text-white text-xs footer__link ">
                     Home
                   </Link>
                 </div>
-                <div className="col-2 col-xl-1 text-center">
+                <div className="col-4 px-1 text-center mb-4">
                   <Link
                     to="/clients"
-                    className="text-white text-xs footer__link"
+                    className="text-white text-xs footer__link "
                   >
                     Clientele
                   </Link>
                 </div>
-                <div className="col-2 col-xl-1 text-center">
+                <div className="col-4 px-1 text-center mb-4">
                   <Link
                     to="/founder"
-                    className="text-white text-xs footer__link"
+                    className="text-white text-xs footer__link "
                   >
                     Founder
                   </Link>
                 </div>
-                <div className="col-2 text-center d-xl-none">
+                <div className="col-4 px-1 text-center mb-4">
                   <Link
                     to="/how-we-work"
-                    className="text-white text-xs footer__link"
+                    className="text-white text-xs footer__link "
                   >
                     How we work
                   </Link>
                 </div>
-                <div className="col-2 col-xl-1 mx-2 px-0 text-center d-none d-xl-flex justofy-content-center align-items-center">
-                  <Link
-                    to="/how-we-work"
-                    className="text-white text-center w-100 m-auto text-xs footer__link"
-                  >
-                    How we work
-                  </Link>
-                </div>
-                <div className="col-2 col-xl-1 text-center">
-                  <Link
-                    to="/levels"
-                    className="text-white text-xs footer__link"
-                  >
+                <div className="col-4 px-1 text-center mb-4">
+                  <Link to="/levels" className="text-white text-xs footer__link ">
                     Levels
                   </Link>
                 </div>
-                <div className="col-2 col-xl-1 text-center">
-                  <Link to="/blog" className="text-white text-xs footer__link">
+                <div className="col-4 px-1 text-center mb-4">
+                  <Link to="/blog" className="text-white text-xs footer__link ">
                     Blog
                   </Link>
                 </div>
-                <div className="col-2 col-xl-1 text-center">
+                <div className="col-4 px-1 text-center mb-4">
                   <Link
                     to="/reviews"
-                    className="text-white text-xs footer__link"
+                    className="text-white text-xs footer__link "
                   >
                     Reviews
                   </Link>
                 </div>
-                <div className="col-2 col-xl-1 text-center">
-                  <Link to="/press" className="text-white text-xs footer__link">
+                <div className="col-4 px-1 text-center mb-4">
+                  <Link to="/press" className="text-white text-xs footer__link ">
                     Press
                   </Link>
                 </div>
-                <div className="col-2 col-xl-1 text-center">
+                <div className="col-4 px-1 text-center mb-4">
                   <Link
                     to="/locations"
-                    className="text-white text-xs footer__link"
+                    className="text-white text-xs footer__link "
                   >
                     Locations
                   </Link>
                 </div>
               </div>
             </div>
-          )}
 
-          <div className="col-12 mb-4 text-center">
-            <Link to="/contact" className="btn btn--small btn-white-outline">
-              ENQUIRE
-            </Link>
+            <div className="col-12 text-center mb-3">
+              <button
+                onClick={openModal}
+                className="btn btn--xsmall btn-white-outline newsletter-trigger"
+              >
+                JOIN THE INNER CIRCLE
+              </button>
+            </div>
+
+            <div className="col-12 text-center mb-3">
+              <Link to="/tc" className="text-white text-xs footer__link">
+                Terms and conditions
+              </Link>
+            </div>
+
+            <div className="col-12">
+              <p className="text-xs text-white mb-0 text-center">
+                Copyright © {new Date().getFullYear()} The Echelon Scene
+              </p>
+            </div>
           </div>
 
-          <div className="col-12 mb-3">
-            <div className="row justify-content-around align-items-center">
-              <div className="col-3">
-                <FooterSocialIcons margin={"small"} />
+          {/* desktop */}
+          <div className="row d-none d-md-flex">
+            {location.pathname !== "/" && (
+              <div className="col-12 mb-3">
+                <div className="row justify-content-center">
+                  <div className="col-2 col-xl-1 text-center">
+                    <Link to="/" className="text-white text-xs footer__link">
+                      Home
+                    </Link>
+                  </div>
+                  <div className="col-2 col-xl-1 text-center">
+                    <Link
+                      to="/clients"
+                      className="text-white text-xs footer__link"
+                    >
+                      Clientele
+                    </Link>
+                  </div>
+                  <div className="col-2 col-xl-1 text-center">
+                    <Link
+                      to="/founder"
+                      className="text-white text-xs footer__link"
+                    >
+                      Founder
+                    </Link>
+                  </div>
+                  <div className="col-2 text-center d-xl-none">
+                    <Link
+                      to="/how-we-work"
+                      className="text-white text-xs footer__link"
+                    >
+                      How we work
+                    </Link>
+                  </div>
+                  <div className="col-2 col-xl-1 mx-2 px-0 text-center d-none d-xl-flex justofy-content-center align-items-center">
+                    <Link
+                      to="/how-we-work"
+                      className="text-white text-center w-100 m-auto text-xs footer__link"
+                    >
+                      How we work
+                    </Link>
+                  </div>
+                  <div className="col-2 col-xl-1 text-center">
+                    <Link
+                      to="/levels"
+                      className="text-white text-xs footer__link"
+                    >
+                      Levels
+                    </Link>
+                  </div>
+                  <div className="col-2 col-xl-1 text-center">
+                    <Link to="/blog" className="text-white text-xs footer__link">
+                      Blog
+                    </Link>
+                  </div>
+                  <div className="col-2 col-xl-1 text-center">
+                    <Link
+                      to="/reviews"
+                      className="text-white text-xs footer__link"
+                    >
+                      Reviews
+                    </Link>
+                  </div>
+                  <div className="col-2 col-xl-1 text-center">
+                    <Link to="/press" className="text-white text-xs footer__link">
+                      Press
+                    </Link>
+                  </div>
+                  <div className="col-2 col-xl-1 text-center">
+                    <Link
+                      to="/locations"
+                      className="text-white text-xs footer__link"
+                    >
+                      Locations
+                    </Link>
+                  </div>
+                </div>
               </div>
+            )}
 
-              <div className="col-3 text-center">
-                                         <button
-              onClick={openModal}
-              className="btn btn--xsmall btn-white-outline newsletter-trigger"
-            >
-              NEWSLETTER
-            </button>
+            <div className="col-12 mb-4 text-center">
+              <Link to="/contact" className="btn btn--small btn-white-outline">
+                ENQUIRE
+              </Link>
+            </div>
+
+            <div className="col-12 mb-3">
+              <div className="row justify-content-around align-items-center">
+                <div className="col-3">
+                  <FooterSocialIcons margin={"small"} />
+                </div>
+
+                <div className="col-3 text-center">
+                  <button
+                    onClick={openModal}
+                    className="text-white text-xs newsletter-trigger"
+                    style={{ background: 'none', border: 'none', cursor: 'pointer' }}
+                  >
+                    Newsletter
+                  </button>
+                </div>
+
+                <div className="col-3 text-center">
+                  <Link to="/tc" className="text-white text-xs">
+                    Terms and conditions
+                  </Link>
+                </div>
+
+                <div className="col-3 text-right">
+                  <p className="text-xs text-white mb-0">
+                    Copyright © {new Date().getFullYear()} The Echelon Scene
+                  </p>
+                </div>
+
               </div>
-
-              <div className="col-3 text-center">
-                <Link to="/tc" className="text-white text-xs">
-                  Terms and conditions
-                </Link>
-              </div>
-
-              <div className="col-3 text-right">
-                <p className="text-xs text-white mb-0">
-                  Copyright © {new Date().getFullYear()} The Echelon Scene
-                </p>
-              </div>
-
             </div>
           </div>
         </div>
-      </div>
       </div>
 
       <NewsletterModal isOpen={isOpen} onClose={closeModal} />
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -27,64 +27,62 @@ export const Navbar: React.FC = (): JSX.Element => {
           {visible ? <MenuCloseIcon /> : <MenuIcon />}
         </button>
 
-      <div
-        className={
-          visible
-            ? "navbar-mobile__main shadow-xl navbar-mobile__main--visible"
-            : "navbar-mobile__main shadow-xl"
-        }
-      >
-        <ul className="navbar-nav px-5">
-          <NavbarLink url="/" name="Home" onClick={onClickMenuButton} />
-          <NavbarLink
-            url="/clients"
-            name="Clientele"
-            onClick={onClickMenuButton}
-          />
-          <NavbarLink
-            url="/founder"
-            name="Founder"
-            onClick={onClickMenuButton}
-          />
-          <NavbarLink
-            url="/how-we-work"
-            name="How we work"
-            onClick={onClickMenuButton}
-          />
-          <NavbarLink url="/levels" name="Levels" onClick={onClickMenuButton} />
-          <NavbarLink url="/blog" name="Blog" onClick={onClickMenuButton} />
-          <NavbarLink
-            url="/reviews"
-            name="Reviews"
-            onClick={onClickMenuButton}
-          />
-          <NavbarLink url="/press" name="Press" onClick={onClickMenuButton} />
-          <NavbarLink
-            url="/locations"
-            name="Locations"
-            onClick={onClickMenuButton}
-          />
-          <Link
-            to="/contact"
-            className="btn btn--small btn-white-outline shadow w-auto navbar-mobile__contact-button"
-            onClick={onClickMenuButton}
-          >
-            CONTACT
-          </Link>
-          <button
-            onClick={() => {
-              openModal();
-              onClickMenuButton();
-            }}
-            className="btn btn--small btn-white-outline shadow w-auto navbar-mobile__contact-button mt-3 newsletter-trigger"
-          >
-            JOIN THE NEWSLETTER
-          </button>
-        </ul>
-      </div>
+        <div
+          className={
+            visible
+              ? "navbar-mobile__main shadow-xl navbar-mobile__main--visible"
+              : "navbar-mobile__main shadow-xl"
+          }
+        >
+          <ul className="navbar-nav px-5">
+            <NavbarLink url="/" name="Home" onClick={onClickMenuButton} />
+            <NavbarLink
+              url="/clients"
+              name="Clientele"
+              onClick={onClickMenuButton}
+            />
+            <NavbarLink
+              url="/founder"
+              name="Founder"
+              onClick={onClickMenuButton}
+            />
+            <NavbarLink
+              url="/how-we-work"
+              name="How we work"
+              onClick={onClickMenuButton}
+            />
+            <NavbarLink url="/levels" name="Levels" onClick={onClickMenuButton} />
+            <NavbarLink url="/blog" name="Blog" onClick={onClickMenuButton} />
+            <NavbarLink
+              url="/reviews"
+              name="Reviews"
+              onClick={onClickMenuButton}
+            />
+            <NavbarLink url="/press" name="Press" onClick={onClickMenuButton} />
+            <NavbarLink
+              url="/locations"
+              name="Locations"
+              onClick={onClickMenuButton}
+            />
+            <li
+              className={"navbar-mobile__item"}
+              style={{ cursor: "pointer" }}
+              onClick={() => openModal()}
+            >
+              <a href='#'>Newsletter</a>
+            </li>
+            <Link
+              to="/contact"
+              className="btn btn--small btn-white-outline shadow w-auto navbar-mobile__contact-button"
+              onClick={onClickMenuButton}
+            >
+              CONTACT
+            </Link>
+          </ul>
+        </div>
 
-      <NewsletterModal isOpen={isOpen} onClose={closeModal} />
-    </nav>
+        <NewsletterModal isOpen={isOpen} onClose={closeModal} />
+      </nav>
     </>
   );
 };
